41c89c135b475b28a9d79466ee9afd1c6c76fb3d
MariaP78 <maria_minodora07@yahoo.com>
2021-07-29 14:48:48 +0300

    Delete Button Works + automatically refreshed + pop up with "Product successfully deleted" appears when product is deleted successfully

--- online-shop/src/app/feature/products/table-container/table-container.component.ts
+++ online-shop/src/app/feature/products/table-container/table-container.component.ts
@@ -30,7 +30,9 @@ export class TableContainerComponent implements OnInit {
       () => {
         this.loadProducts();
         //console.log('s-a sters cu succes produsul cu id: ' + id);
-        this._snackBar.open('Product successfully deleted');
+        this._snackBar.open(
+          'Product with id ' + id + ' was successfully deleted'
+        );
       },
       (error) => this._snackBar.open(error)
     );
