2f3968e28e54914b3ed8ce10e4eba87d15bb6969
MariaP78 <maria_minodora07@yahoo.com>
2021-07-29 12:23:46 +0300

    Added new Components Created table with products using Material UI

--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableContainerComponent } from './feature/products/table-container/table-container.component';
+import { TableComponent } from './feature/products/table/table.component';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableContainerComponent,
+    TableComponent
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
     AppRoutingModule
   ],
